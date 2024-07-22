909f1abfe9ca1206a2f86f44dd0f50f46cfff186
David Runger <davidjrunger@gmail.com>
2024-07-22 12:02:31 -0500

    Delete elided email tests
    I don't care what was deleted; I just care what was preserved. Testing
    the elided part of the emails is, I think, a not worthwhile hassle.

--- test/test_email_reply_trimmer.rb
+++ test/test_email_reply_trimmer.rb
@@ -6,7 +6,6 @@ require 'runger_email_reply_trimmer'
 class TestRungerEmailReplyTrimmer < Minitest::Test
   EMAILS  = Dir['test/emails/*.txt'].map  { |path| File.basename(path) }
   TRIMMED = Dir['test/trimmed/*.txt'].map { |path| File.basename(path) }
-  ELIDED  = Dir['test/elided/*.txt'].map { |path| File.basename(path) }
 
   EMBEDDED_EMAILS = %w[
     email_headers_1
@@ -24,10 +23,6 @@ class TestRungerEmailReplyTrimmer < Minitest::Test
     assert_equal(EMAILS, TRIMMED, 'Files in /emails and /trimmed folders should match 1-to-1.')
   end
 
-  def test_all_emails_have_a_matching_elided
-    assert_equal(EMAILS, ELIDED, 'Files in /emails and /elided folders should match 1-to-1.')
-  end
-
   def test_normalize_line_endings_email_has_windows_line_endings
     assert_match(/\r\n/, File.read('test/emails/normalize_line_endings.txt'))
   end
@@ -40,7 +35,6 @@ class TestRungerEmailReplyTrimmer < Minitest::Test
     name = File.basename(filename, '.txt')
     define_method("test_#{name}") do
       assert_equal(trimmed(filename), trim(filename), "[TRIMMED] EMAIL: #{filename}")
-      assert_equal(elided(filename), elide(filename), "[ELIDED] EMAIL: #{filename}")
     end
   end
 
@@ -57,10 +51,6 @@ class TestRungerEmailReplyTrimmer < Minitest::Test
     RungerEmailReplyTrimmer.trim(email(filename))
   end
 
-  def elide(filename)
-    RungerEmailReplyTrimmer.trim(email(filename), true)[1]
-  end
-
   def extract_embedded_email(filename)
     RungerEmailReplyTrimmer.extract_embedded_email(email(filename))
   end
@@ -71,10 +61,6 @@ class TestRungerEmailReplyTrimmer < Minitest::Test
 
   def trimmed(filename)
     File.read("test/trimmed/#{filename}").strip
-  end
-
-  def elided(filename)
-    File.read("test/elided/#{filename}").strip
   end
 
   def embedded(filename)
